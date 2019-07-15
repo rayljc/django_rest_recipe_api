a9b3d56ec6277eb7d05c64617565d1209e903962
rayjclin <nturay0321@gmail.com>
2019-07-16 00:44:34 +0800

    why

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,14 +16,3 @@ class ModelTest(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    def test_normalize_user_email(self):
-        """Test normalizing email to lower case"""
-        email = 'test@GMAIL.COM'
-        password = 'Pwerr123445'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
-
-        self.assertEqual(user.email, email.lower())
-
