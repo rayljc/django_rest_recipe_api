86ac54872de8d280368836cde076cd0e754fecca
rayjclin <nturay0321@gmail.com>
2019-08-16 00:16:51 -0400

    fix W293 issue

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,7 +22,7 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
-        
+
         return queryset.filter(
             user=self.request.user
         ).order_by('-name').distinct()
